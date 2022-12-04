bbe67ddad680713ec0ffe23e8101fced7ff57f55
youngprof-of-tech <officialmosesatteh@gmail.com>
2022-12-04 11:24:51 +0100

    modify html css and javascript codes

--- public/index.js
+++ public/index.js
@@ -1,75 +1,61 @@
 "use strict";
-/**navvar toggle */
-const navOpenBtn = document.querySelector("[data-nav-open-btn]");
-const navbar = document.querySelector("[data-navbar]");
-const navCloseBtn = document.querySelector("[data-nav-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
-
-const elemArr = [navCloseBtn, overlay, navOpenBtn];
-for (let i = 0; i < elemArr.length; i++) {
-  elemArr[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]"),
+  navbar = document.querySelector("[data-navbar]"),
+  navCloseBtn = document.querySelector("[data-nav-close-btn]"),
+  overlay = document.querySelector("[data-overlay]"),
+  elemArr = [navCloseBtn, overlay, navOpenBtn];
+for (let e = 0; e < elemArr.length; e++)
+  elemArr[e].addEventListener("click", function () {
+    navbar.classList.toggle("active"), overlay.classList.toggle("active");
   });
-}
-/**toggle navbar & overlay when click any navbar-link */
 const navbarLinks = document.querySelectorAll("[data-navbar-link]");
-for (let i = 0; i < navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
+for (let e = 0; e < navbarLinks.length; e++)
+  navbarLinks[e].addEventListener("click", function () {
+    navbar.classList.toggle("active"), overlay.classList.toggle("active");
   });
-}
-
-const sliderElm = document.querySelector(".slider-container .slider");
-const btnLeft = document.querySelector(".slider-container .btn-left");
-const btnRight = document.querySelector(".slider-container .btn-right");
-
-const numberSliderBoxs = sliderElm.children.length;
+const sliderElm = document.querySelector(".slider-container .slider"),
+  btnLeft = document.querySelector(".slider-container .btn-left"),
+  btnRight = document.querySelector(".slider-container .btn-right"),
+  numberSliderBoxs = sliderElm.children.length;
 let idxCurrentSlide = 0;
-
-// Functions:
 function moveSlider() {
-  let leftMargin = (sliderElm.clientWidth / numberSliderBoxs) * idxCurrentSlide;
-  sliderElm.style.marginLeft = -leftMargin + "px";
-  console.log(sliderElm.clientWidth, leftMargin);
+  let e = (sliderElm.clientWidth / numberSliderBoxs) * idxCurrentSlide;
+  (sliderElm.style.marginLeft = -e + "px"),
+    console.log(sliderElm.clientWidth, e);
 }
 function moveLeft() {
-  if (idxCurrentSlide === 0) idxCurrentSlide = numberSliderBoxs - 1;
-  else idxCurrentSlide--;
-
-  moveSlider();
+  0 === idxCurrentSlide
+    ? (idxCurrentSlide = numberSliderBoxs - 1)
+    : idxCurrentSlide--,
+    moveSlider();
 }
 function moveRight() {
-  if (idxCurrentSlide === numberSliderBoxs - 1) idxCurrentSlide = 0;
-  else idxCurrentSlide++;
-
-  moveSlider();
+  idxCurrentSlide === numberSliderBoxs - 1
+    ? (idxCurrentSlide = 0)
+    : idxCurrentSlide++,
+    moveSlider();
 }
-
-// Event Listeners:
-btnLeft.addEventListener("click", moveLeft);
-btnRight.addEventListener("click", moveRight);
-window.addEventListener("resize", moveSlider);
-
-(function () {
-  var options = {
-    whatsapp: "+2348146367092", // WhatsApp number
-    call_to_action: "Message us", // Call to action
-    button_color: "#FF6550", // Color of button
-    position: "left", // Position may be 'right' or 'left'
-    pre_filled_message: "Hi, I would like to make enquiry ?", // WhatsApp pre-filled message
-  };
-  var proto = "https:",
-    host = "getbutton.io",
-    url = proto + "//static." + host;
-  var s = document.createElement("script");
-  s.type = "text/javascript";
-  s.async = true;
-  s.src = url + "/widget-send-button/js/init.js";
-  s.onload = function () {
-    WhWidgetSendButton.init(host, proto, options);
-  };
-  var x = document.getElementsByTagName("script")[0];
-  x.parentNode.insertBefore(s, x);
-})();
+btnLeft.addEventListener("click", moveLeft),
+  btnRight.addEventListener("click", moveRight),
+  window.addEventListener("resize", moveSlider),
+  (function () {
+    var e = {
+        whatsapp: "+2348146367092",
+        call_to_action: "Message us",
+        button_color: "#FF6550",
+        position: "left",
+        pre_filled_message: "Hi, I would like to make enquiry ?",
+      },
+      t = "https:",
+      n = "getbutton.io",
+      r = t + "//static." + n,
+      i = document.createElement("script");
+    (i.type = "text/javascript"),
+      (i.async = !0),
+      (i.src = r + "/widget-send-button/js/init.js"),
+      (i.onload = function () {
+        WhWidgetSendButton.init(n, t, e);
+      });
+    var l = document.getElementsByTagName("script")[0];
+    l.parentNode.insertBefore(i, l);
+  })();
